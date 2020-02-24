b4748b952515692b2e6137fba7abdd964b4390f8
Joseph Hand <12785608+popa6200@users.noreply.github.com>
2020-02-24 12:48:15 -0700

    Revert "Revert "Pneumatics Subsystem""

---
+++ src/main/java/frc/robot/commands/ToggleIntakeSolenoid.java
@@ -0,0 +1,43 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Pneumatics;
+
+public class ToggleIntakeSolenoid extends CommandBase {
+  private final Pneumatics m_pnSubsystem;
+
+  public ToggleIntakeSolenoid(Pneumatics pnSubsystem) {
+    m_pnSubsystem = pnSubsystem;
+
+    addRequirements(m_pnSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    m_pnSubsystem.toggleIntakeSolenoid();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return true; //Exits immediately after called, the solenoid 'latches' the piston state and stays.
+  }
+}
